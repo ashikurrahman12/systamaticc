98167139d5899be73493ff8da313c4c33c785eaa
ashikurrahman12 <74830708+ashikurrahman12@users.noreply.github.com>
2020-11-21 22:58:32 +0600

    Add files via upload

---
+++ psd to html/html/js/main.js
@@ -0,0 +1,8 @@
+// BX _SLIDER//
+$(document).ready(function(){
+    $('.slider_aria').bxSlider();
+    jQuery('#main_menu').meanmenu({
+      meanMenuContainer:'.mobile_menu',
+      meanScreenWidth:991,
+    });
+  });
